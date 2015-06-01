1bc77ac0c8627fd3a72bce02ae172e0a54e52205
Andy Aylward <aaylward@gmail.com>
2015-06-01 00:25:31 -0400

    add my-map and a couple more tests

--- clojure_fun/src/clojure_fun/sequences.clj
+++ clojure_fun/src/clojure_fun/sequences.clj
@@ -5,3 +5,8 @@
     (if (empty? in)
       out
       (recur (rest in) (cons (first in) out)))))
+
+(defn my-map [func col]
+  (if (empty? col)
+    '()
+    (lazy-seq (cons (func (first col)) (my-map func (rest col))))))
