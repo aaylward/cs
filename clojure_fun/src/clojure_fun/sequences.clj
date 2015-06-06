bd161f31e5796e71b9e1a161f10f2b801305c345
Andy Aylward <aaylward@gmail.com>
2015-06-06 17:22:27 -0400

    add nth function

--- clojure_fun/src/clojure_fun/sequences.clj
+++ clojure_fun/src/clojure_fun/sequences.clj
@@ -1,5 +1,14 @@
 (ns clojure-fun.sequences)
 
+(defn my-nth [input n]
+  "eats it on negative `n`"
+  (loop [in input counter n]
+    (if (empty? in)
+      nil
+    (if (= counter 0)
+      (first in)
+      (recur (rest in) (dec counter))))))
+
 (defn my-reverse [input]
   (loop [in input out '()]
     (if (empty? in)
