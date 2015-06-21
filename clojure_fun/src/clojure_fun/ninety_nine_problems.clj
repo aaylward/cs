cdf040f3f5374818946291a0d9b0891d5918ce7a
Andy Aylward <aaylward@gmail.com>
2015-06-20 23:57:00 -0400

    label the problems

--- clojure_fun/src/clojure_fun/ninety_nine_problems.clj
+++ clojure_fun/src/clojure_fun/ninety_nine_problems.clj
@@ -1,30 +1,36 @@
 (ns clojure-fun.ninety-nine-problems)
 
+;(*) Find the last element of a list.
 (defn my-last [input]
   (first (reverse input)))
 
+;(*) Find the last but one element of a list.
 (defn my-but-last [input]
   (first (rest (reverse input))))
 
-(defn my-nth [input n]
+;(*) Find the K'th element of a list.
+(defn my-kth [input n]
   "indexed from 0"
   (loop [in input k n]
     (if (= k 0)
       (first in)
       (recur (rest in) (dec k)))))
 
+;(*) Find the number of elements of a list.
 (defn my-length [input]
   (loop [in input size 0]
     (if (empty? in)
       size
       (recur (rest in) (inc size)))))
 
+;(*) Reverse a list.
 (defn my-reverse [input]
   (loop [in input out '()]
     (if (empty? in)
       out
       (recur (rest in) (cons (first in) out)))))
 
+;(*) Find out whether a list is a palindrome.
 (defn my-palindrome [input]
   (= input (my-reverse input)))
 
